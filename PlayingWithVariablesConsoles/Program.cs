15b06f1de6880e66f77a6a86e40a60bab877c5a0
dfish19 <dfish19@yahoo.com>
2022-08-03 18:30:42 -0500

    initial commit

---
+++ PlayingWithVariablesConsoles/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PlayingWithVariablesConsoles
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int age = 40;
+            string name = "Fish";
+            bool isAlive = true;
+            double fists = 2;
+            decimal weight = 10.5m;
+
+            Console.WriteLine($"I am {age} , and my name is  {name}.  I have  {fists} fists  weighing  {weight} pounds,  and that is  {isAlive}. ");
+        }
+    }
+}
+    
